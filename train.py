e4910f5d18ec85159f642ad37125c45900b42bc7
sofi <diksik007@gmail.com>
2019-03-10 14:28:34 +0300

    Update train.py

--- train.py
+++ train.py
@@ -25,7 +25,9 @@ use_cuda = False
 pretrained = False
 
 if use_cuda:
-    gpu = 0
+    device = 'cuda:0'
+else:
+    device = 'cpu'
 
 NAME = 'exp0'
 DATASET = 'mnist'
@@ -34,16 +36,16 @@ DATA_PATH = '/home/rkhairulin/data/bwgan/'
 if DATASET.lower() == 'mnist':
     IMG_SIZE = 28
     CH = 1
-    from model_mnist import *
+    from model_mnist import Generator, Discriminator
 else:
     IMG_SIZE  = 32
     CH = 3
-    from model import *
+    from model import Generator, Discriminator
 
 DIM = 64  # Model dimensionality
 Z_SIZE = 128
 BATCH_SIZE = 60
-OUTPUT_DIM = int(IMG_SIZE * IMG_SIZE)
+OUTPUT_DIM = int(IMG_SIZE * IMG_SIZE * CH)
 CRITIC_ITERS = 2  # For WGAN and WGAN-GP, number of critic iters per gen iter
 LAMBDA = 10  # Gradient penalty lambda hyperparameter
 ITERS = 200000  # How many generator iterations to train for
@@ -84,7 +86,7 @@ def sobolev_transform(x, c=5, s=1):
     Y = Y[None, None]
 
     # computing the scale (1 + |\xi|^2)^{s/2}
-    scale = (1 + c * (X**2 + Y**2))**(s/2)
+    scale = ((1 + c * (X**2 + Y**2))**(s/2)).to(device)
 
     # scale is a real number which scales both real and imaginary parts by multiplying
     scale = torch.stack([scale, scale], -1)
@@ -115,19 +117,16 @@ def inf_train_gen():
 
 
 def calc_gradient_penalty(D, real_data, fake_data, gamma, lamb):
-    eps = torch.rand(BATCH_SIZE, 1)
+    eps = torch.rand(BATCH_SIZE, 1).to(device)
     real_data = real_data.view((BATCH_SIZE, -1))
-    eps = eps.cuda(gpu) if use_cuda else eps
 
-    interpolates = eps * real_data + ((1 - eps) * fake_data)
-    interpolates = interpolates.cuda(gpu) if use_cuda else interpolates
+    interpolates = (eps * real_data + ((1 - eps) * fake_data)).to(device)
     interpolates = autograd.Variable(interpolates, requires_grad=True)
 
     disc_interpolates = D(interpolates)
 
     gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
-                              grad_outputs=torch.ones(disc_interpolates.size()).cuda(gpu) if use_cuda else torch.ones(
-                                  disc_interpolates.size()),
+                              grad_outputs=torch.ones(disc_interpolates.size()).to(device),
                               create_graph=True, retain_graph=True, only_inputs=True)[0]
     gradients = gradients.view((BATCH_SIZE, CH, IMG_SIZE, IMG_SIZE))
     dual_sobolev_gradients = sobolev_transform(gradients, C, -S)
@@ -135,19 +134,15 @@ def calc_gradient_penalty(D, real_data, fake_data, gamma, lamb):
     return gradient_penalty
 
 if pretrained:
-    netG = Generator()
+    netG = Generator().to(device)
     netG.load_state_dict(torch.load(os.path.join(DATA_PATH, experiment_path, 'generator.pth.tar'))['state_dict'])
-    netD = Discriminator()
+    netD = Discriminator().to(device)
     netD.load_state_dict(torch.load(os.path.join(DATA_PATH, experiment_path, 'discriminator.pth.tar'))['state_dict'])
 else:
-    netG = Generator(DIM, OUTPUT_DIM)
-    netD = Discriminator(DIM)
+    netG = Generator(DIM, OUTPUT_DIM).to(device)
+    netD = Discriminator(DIM).to(device)
 print(netG)
 print(netD)
-
-if use_cuda:
-    netD = netD.cuda(gpu)
-    netG = netG.cuda(gpu)
 
 optimizerD = optim.Adam(netD.parameters(), lr=LEARNING_RATE, betas=(BETA[0], BETA[1]))
 optimizerG = optim.Adam(netG.parameters(), lr=LEARNING_RATE, betas=(BETA[0], BETA[1]))
@@ -160,11 +155,8 @@ else:
 schedulerD = optim.lr_scheduler.LambdaLR(optimizerD, decay)
 schedulerG = optim.lr_scheduler.LambdaLR(optimizerG, decay)
 
-one = torch.FloatTensor([1])[0]
-mone = one * -1
-if use_cuda:
-    one = one.cuda(gpu)
-    mone = mone.cuda(gpu)
+one = torch.FloatTensor([1])[0].to(device)
+mone = (one * -1).to(device)
 
 # Dataset iterator
 train_gen, dev_gen = dataloader(DATASET, DATA_PATH, batch_size=BATCH_SIZE, img_size=IMG_SIZE)
@@ -179,9 +171,7 @@ for iteration in range(ITERS):
 
     for iter_d in range(CRITIC_ITERS):
         _data = next(data)
-        real_data = torch.Tensor(_data)
-        if use_cuda:
-            real_data = real_data.cuda(gpu)
+        real_data = torch.Tensor(_data).to(device)
         real_data_v = autograd.Variable(real_data)
 
         netD.zero_grad()
@@ -192,9 +182,7 @@ for iteration in range(ITERS):
         D_real.backward(mone)
 
         # train with fake
-        noise = torch.randn(BATCH_SIZE, Z_SIZE)
-        if use_cuda:
-            noise = noise.cuda(gpu)
+        noise = torch.randn(BATCH_SIZE, Z_SIZE).to(device)
         noisev = autograd.Variable(noise, volatile=True)  # totally freeze netG
         fake = autograd.Variable(netG(noisev).data)
         inputv = fake
@@ -215,9 +203,7 @@ for iteration in range(ITERS):
         p.requires_grad = False
     netG.zero_grad()
 
-    noise = torch.randn(BATCH_SIZE, Z_SIZE)
-    if use_cuda:
-        noise = noise.cuda(gpu)
+    noise = torch.randn(BATCH_SIZE, Z_SIZE).to(device)
     noisev = autograd.Variable(noise)
     fake = netG(noisev)
     G = netD(fake)
@@ -229,13 +215,11 @@ for iteration in range(ITERS):
     schedulerG.step()
     schedulerD.step()
 
-    print('iteration ', iteration)
+    # print('iteration ', iteration)
     if iteration % 100 == 99:
         dev_disc_costs = []
         for images, _ in dev_gen:
-            imgs = torch.Tensor(images)
-            if use_cuda:
-                imgs = imgs.cuda(gpu)
+            imgs = torch.Tensor(images).to(device)
             imgs_v = autograd.Variable(imgs, volatile=True)
 
             D = netD(imgs_v)
@@ -246,8 +230,7 @@ for iteration in range(ITERS):
         writer.add_scalar('loss/dev_disc_cost', np.mean(dev_disc_costs), iteration)
 
         # logging generated image to tensorboardX
-        z = torch.randn((BATCH_SIZE, Z_SIZE))
-        z = z.cuda() if use_cuda else z
+        z = torch.randn((BATCH_SIZE, Z_SIZE)).to(device)
         ims = netG(z).reshape(BATCH_SIZE, CH, IMG_SIZE, IMG_SIZE)
         if DATASET.lower() == 'mnist':
             ims = torch.stack([ims, ims, ims], dim=1)
@@ -255,12 +238,12 @@ for iteration in range(ITERS):
         x = vutils.make_grid(ims[:n], nrow=int(np.sqrt(n)), normalize=True, range=(0, 1))
         writer.add_image('generated_{}_{}'.format(DATASET, NAME), x, iteration)
 
-        # lib.generate_sample(netG, BATCH_SIZE, Z_SIZE, use_cuda=use_cuda)
         # clear_output()
+        # lib.generate_sample(netG, BATCH_SIZE, Z_SIZE, (CH, IMG_SIZE), device=device)
 
 writer.close()
 # Generate dataset of images to feed them to FID scorer
-lib.save_dataset(netG, BATCH_SIZE, Z_SIZE, IMG_SIZE, DATA_PATH, name=DATASET + '-' + NAME, N=N, use_cuda=use_cuda)
+lib.save_dataset(netG, BATCH_SIZE, Z_SIZE, (CH, IMG_SIZE), DATA_PATH, name=DATASET + '-' + NAME, N=N, device=device)
 # Save models
 torch.save({'state_dict': netG.state_dict()}, os.path.join(DATA_PATH, experiment_path, 'generator.pth.tar'))
 torch.save({'state_dict': netD.state_dict()}, os.path.join(DATA_PATH, experiment_path, 'discriminator.pth.tar'))
